ccf0292d8c02b65b027223f3686db07bfd48368a
Rogério Ramos da Silva <habutre+github@gmail.com>
2019-05-07 08:46:56 +0200

    Change offset reset to latest

--- lib/kafka_elixir_lab/application.ex
+++ lib/kafka_elixir_lab/application.ex
@@ -21,7 +21,8 @@ defmodule KafkaElixirLab.Application do
           [
             commit_interval: 5000,
             commit_threshold: 100,
-            auto_offset_reset: :earliest,
+            enable_auto_commit: true,
+            auto_offset_reset: :latest,
             heartbeat_interval: 1_000
           ]
         ]
